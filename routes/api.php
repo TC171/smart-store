a9762cf2630f50f9b1f4bb2b8583319df1283629
Ba hai <hainbph53579@gmail.com>
2026-03-26 22:55:56 +0700

    add search controller

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,13 @@
 use App\Http\Controllers\Frontend\HomeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Frontend\SearchController;
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
 Route::get('/products', [HomeController::class, 'loadProducts']);
+
+Route::get('/search', [SearchController::class, 'search']);
